58788011d06f47f35d1c79fb7ac52a820899d8a8
e-Yasmina <102832353+e-Yasmina@users.noreply.github.com>
2023-09-04 16:52:26 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,56 +1,47 @@
-import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
-import pandas as pd
-import plotly.express as px
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+
+from dash import Dash, html, dcc
+
+app = Dash(__name__)
 server=app.server
 
-# Load your Excel data
-file_path = "90A5446B6867-ferme anoljdidÀúÊ·Êý¾Ý (3).xls"
-df = pd.read_excel(file_path)
-
-# Define the layout of your dashboard
+  
 app.layout = html.Div([
-    html.H1("Farm Data Dashboard"),
+    html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
 
-    # Dropdown to select a device
-    dcc.Dropdown(
-        id='device-dropdown',
-        options=[{'label': device, 'value': device} for device in df['Device Name'].unique()],
-        value=df['Device Name'].unique()[0],
-        multi=False
-    ),
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
+                     ['Montréal', 'San Francisco'],
+                     multi=True),
 
-    # Dropdown to select a variable
-    dcc.Dropdown(
-        id='variable-dropdown',
-        options=[{'label': variable, 'value': variable} for variable in df.columns if variable not in ['Number', 'Device Name', 'Device Number', 'Receive Time']],
-        value='temperature',  # Default variable to display
-        multi=False
-    ),
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+    ], style={'padding': 10, 'flex': 1}),
 
-    # Line chart for the selected variable
-    dcc.Graph(id='selected-variable-graph'),
-])
+    html.Div(children=[
+        html.Label('Checkboxes'),
+        dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
+                      ['Montréal', 'San Francisco']
+        ),
 
-# Define callback to update the graph based on device and variable selection
-@app.callback(
-    Output('selected-variable-graph', 'figure'),
-    [Input('device-dropdown', 'value'),
-     Input('variable-dropdown', 'value')]
-)
-def update_graph(selected_device, selected_variable):
-    filtered_df = df[df['Device Name'] == selected_device]
+        html.Br(),
+        html.Label('Text Input'),
+        dcc.Input(value='MTL', type='text'),
 
-    # Create a line chart for the selected variable
-    variable_graph = px.line(filtered_df, x='Receive Time', y=selected_variable, title=f'{selected_variable} over Time')
+        html.Br(),
+        html.Label('Slider'),
+        dcc.Slider(
+            min=0,
+            max=9,
+            marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
+            value=5,
+        ),
+    ], style={'padding': 10, 'flex': 1})
+], style={'display': 'flex', 'flex-direction': 'row'})
 
-    return variable_graph
-
-# Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
