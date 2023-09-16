cd684e79b7ccf1946fd6f5d35ce941ec4f32c1b4
e-Yasmina <102832353+e-Yasmina@users.noreply.github.com>
2023-09-16 22:42:37 +0100

    Update dash_components.py

--- dash_components.py
+++ dash_components.py
@@ -1,3 +1,16 @@
+from dash import dcc, html
+import time
+import pathlib
+
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import State, Input, Output
+import dash_daq as daq
+
+
+
 temperature = html.Div(
     id="control-panel-temperature",
     children=[
